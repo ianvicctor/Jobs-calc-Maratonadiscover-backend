a91e662542d347f7b0b52ac3d2e0c94d23ca436e
Ian Victor <ian_valadares18@hotmail.com>
2021-04-25 23:41:09 -0300

    refactoring...

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -6,10 +6,20 @@ module.exports = {
   index(req, res) {
     const jobs = Job.get();
     const profile = Profile.get();
+
+    let statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
     const updatedJobs = jobs.map((job) => {
       //ajustes no job
       const remaining = JobUtils.remainingDays(job);
       const status = remaining <= 0 ? "done" : "progress";
+
+      //somando a quantidade de status
+      statusCount[status] += 1;
 
       //cria um array novo e coloca as novas informações: remaining, status e budget
       return {
@@ -20,6 +30,6 @@ module.exports = {
       };
     });
 
-    return res.render("index", { jobs: updatedJobs, profile: profile });
+    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount });
   },
 };
